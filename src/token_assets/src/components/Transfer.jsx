04297f73568d1851983696d5d180e8fca818dc94
MonMaramba <monmaramba@yahoo.com>
2022-06-01 18:21:12 -0700

    completed transfer functionality plus some ux items

--- src/token_assets/src/components/Transfer.jsx
+++ src/token_assets/src/components/Transfer.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import { setMaxListeners } from 'events';
+import React, { useState } from 'react';
+import { Principal } from '@dfinity/principal';
+import { token } from '../../../declarations/token';
 
 function Transfer() {
-  
+  const [recipientId, setId] = useState('');
+  const [amount, setAmount] = useState('');
+  const [isDisabled, setDisable] = useState(false);
+  const [feedback, setFeedback] = useState('');
+  const [isHidden, setHidden] = useState(true);
+
   async function handleClick() {
-    
+    setHidden(true);
+    setDisable(true);
+    const recipient = Principal.fromText(recipientId);
+    const amountToSend = Number(amount);
+    const result = await token.transfer(recipient, amountToSend);
+    setFeedback(result);
+    setHidden(false);
+    setDisable(false);
   }
 
   return (
-    <div className="window white">
-      <div className="transfer">
+    <div className='window white'>
+      <div className='transfer'>
         <fieldset>
           <legend>To Account:</legend>
           <ul>
             <li>
               <input
-                type="text"
-                id="transfer-to-id"
+                type='text'
+                id='transfer-to-id'
+                value={recipientId}
+                onChange={(e) => setId(e.target.value)}
               />
             </li>
           </ul>
@@ -25,17 +42,20 @@ function Transfer() {
           <ul>
             <li>
               <input
-                type="number"
-                id="amount"
+                type='number'
+                id='amount'
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
               />
             </li>
           </ul>
         </fieldset>
-        <p className="trade-buttons">
-          <button id="btn-transfer" onClick={handleClick} >
+        <p className='trade-buttons'>
+          <button id='btn-transfer' onClick={handleClick} disabled={isDisabled}>
             Transfer
           </button>
         </p>
+        <p hidden={isHidden}>{feedback}</p>
       </div>
     </div>
   );
